13ca383da7e87ee3f8234f15140a6f063355ff3e
ktvexe <ktvexe@gmail.com>
2015-11-08 05:41:07 +0800

    delete some olds

--- .hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ktvexe/kernel_module/hello.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/ktvexe/kernel_module/hello.ko /home/ktvexe/kernel_module/hello.o /home/ktvexe/kernel_module/hello.mod.o
